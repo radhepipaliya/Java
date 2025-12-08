d0c724016859e4af7584eee15fa65ad7e0b2e3a2
~RP <radhepipaliya13112004@gmail.com>
2025-12-08 08:58:31 +0530

    Task on recurion

---
+++ src/com/qspider/Recurssion/Recursion.java
@@ -0,0 +1,16 @@
+package com.qspider.Recurssion;
+
+public class Recursion {
+    public static void main(String[] args){
+        print1toN(10);
+    }
+
+    public static void print1toN(int n){
+        if(n==0){
+            return;
+        }
+        print1toN(n-1);
+        System.out.println(n);
+
+    }
+}
---
+++ src/com/qspider/task/RecursionTask.java
@@ -0,0 +1,44 @@
+package com.qspider.task;
+
+public class RecursionTask {
+    public static void main(String[] args){
+        /*System.out.println(recursionFactorial(5));*/
+        /*System.out.println(recursionFibonacci(10));*/
+        printFactors(10,1);
+    }
+
+    public static int recursionFactorial(int n){
+           if(n>=1){
+               return n*recursionFactorial(n-1);
+           }else{
+               return 1;
+           }
+
+    }
+
+    public static int recursionFibonacci(int n){
+
+        if(n == 0){
+            return 0;
+        }
+
+        if(n==1){
+            return 1;
+        }
+
+        return recursionFibonacci(n-1)+recursionFibonacci(n-2);
+    }
+
+    public static void printFactors(int n,int i){
+
+        if(i>n){
+            return;
+        }
+        if(n % i == 0){
+            System.out.println(i);
+        }
+
+        printFactors(n,i+1);
+
+    }
+}
